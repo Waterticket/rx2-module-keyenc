1431703ea2e31f1e022057f31263ecc989233254
Waterticket <matthew218@naver.com>
2023-01-21 11:56:49 +0000

    first commit

---
+++ lang/ko.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang->cmd_keyenc = '키 암호화/복보화 모듈';
+$lang->cmd_keyenc_general_config = '기본 설정';
+$lang->cmd_keyenc_example_config = '예제 설정';
---
+++ models/example.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Rhymix\Modules\Keyenc\Models;
+
+/**
+ * 키 암호화/복보화 모듈
+ * 
+ * Copyright (c) Waterticket
+ * 
+ * Generated with https://www.poesis.org/tools/modulegen/
+ */
+class Example
+{
+	
+}
